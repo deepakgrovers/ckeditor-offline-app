c60073832fec914f69f8049771da0b28a1c1d00b
DEEPAK GROVER <deepakgrover158@gmail.com>
2021-08-23 19:38:29 +0530

    inital commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # ckeditor-offline-app
+
+A simple HTML editor desktop app using CKEditor and Electron JS.
+
+## how to step and run this app in local system
+- Node.JS must be install is system
+- Open cloned repo and run this command for all project dependencies``` npm i```
+- Last run  ``` npm start``` for launch application 
---
+++ index.js
@@ -0,0 +1,42 @@
+const { app, BrowserWindow} = require('electron')
+const path = require('path')
+const url = require('url')
+let mainWindow
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        nodeIntegration: false,
+        webPreferences: {
+            nodeIntegration: true
+        },
+        show: false
+    });
+    mainWindow.maximize();
+    mainWindow.setMenu(null)
+
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }))
+
+    // mainWindow.webContents.openDevTools()
+
+    mainWindow.on('closed', function () {
+        mainWindow = null
+    })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', function () {
+    if (mainWindow === null) {
+        createWindow()
+    }
+})
